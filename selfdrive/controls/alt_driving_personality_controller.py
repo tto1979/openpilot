52ab2eeed1524361519e9472c893dd228aa1728d
CT921 <tim.chen242@gmail.com>
2024-08-01 10:59:52 +0800

    Update alt_driving_personality_controller.py

--- selfdrive/controls/alt_driving_personality_controller.py
+++ selfdrive/controls/alt_driving_personality_controller.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import time
 from openpilot.common.conversions import Conversions as CV
 from openpilot.common.params import Params
 
 class AlternativeDrivingPersonalityController:
-    def __init__(self):
+    def __init__(self, CP):
         # try except to avoid issue in longitudinal testing
         try:
             self._speed = 40 * CV.KPH_TO_MS
@@ -13,12 +14,15 @@ class AlternativeDrivingPersonalityController:
 
         self._mode = 0
         self._active = False
+        self.CP = CP
         self.params = Params()
         self.old_personality = None
+        self.last_record_time = 0
 
     def update(self, v_ego):
         was_active = self._active
-        self._active = self._speed > 0 and v_ego < self._speed
+        self._active = self._speed > 0 and v_ego < self._speed and self.CP.openpilotLongitudinalControl
+        current_time = time.time()
 
         if self._active and not was_active:
             # Switching to active state
@@ -28,6 +32,11 @@ class AlternativeDrivingPersonalityController:
             # Switching back to inactive state
             if self.old_personality is not None:
                 self.params.put_nonblocking('LongitudinalPersonality', self.old_personality)
+        
+        # Record LongitudinalPersonality every 30 seconds when speed is above 40 km/h
+        if self._active and current_time - self.last_record_time >= 30:
+            self.old_personality = self.params.get('LongitudinalPersonality')
+            self.last_record_time = current_time
 
     def get_personality(self, personality):
         return self._mode if self._active else personality
