603974b974f6720f6e4cf8ca07a271387bbe8a2e
CT921 <tim.chen242@gmail.com>
2024-10-08 22:25:15 +0800

    Update generators.py

--- common/mock/generators.py
+++ common/mock/generators.py
@@ -1,6 +1,12 @@
 from cereal import messaging
 
 
+LOCATION1 = (32.7174, -117.16277)
+LOCATION2 = (32.7558, -117.2037)
+
+LLK_DECIMATION = 10
+
+
 def generate_livePose():
   msg = messaging.new_message('livePose')
   meas = {'x': 0.0, 'y': 0.0, 'z': 0.0, 'xStd': 0.0, 'yStd': 0.0, 'zStd': 0.0, 'valid': True}
--- selfdrive/car/car_specific.py
+++ selfdrive/car/car_specific.py
@@ -123,7 +123,7 @@ class CarSpecificEvents:
           Params().put_bool("LateralAllowed", False)
         self.prev_atl = CS_prev.cruiseState.available
 
-      if self.CS.out.brakehold_governor:
+      if CS.out.brakehold_governor:
         events.add(EventName.automaticBrakehold)
 
     elif self.CP.carName == 'gm':
