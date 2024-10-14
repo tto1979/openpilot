c3dbab9f31f4eb79d0d27e6298439b5b683c980b
CT921 <tim.chen242@gmail.com>
2024-10-14 15:14:05 +0800

    update

--- release/release_files.py
+++ release/release_files.py
@@ -14,7 +14,7 @@ blacklist = [
   "panda/examples/",
   "panda/tests/safety/",
 
-  "selfdrive/car/torque_data/lat_models/*",
+  "opendbc_repo/dbc/car/torque_data/lat_models/*",
 
   "opendbc_repo/dbc/.*.dbc$",
   "opendbc_repo/dbc/generator/",
