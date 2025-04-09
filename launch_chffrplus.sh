5b45355498eeb6ab7745ffb79065de22718bafea
CT921 <tim.chen242@gmail.com>
2025-04-09 14:42:48 +0800

    0.9.8 hotfixes

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -81,20 +81,13 @@ function launch {
 
   python ./force_car_recognition.py
 
-  # TOP require flask
-  if ! python3 -c "import flask" &> /dev/null; then
-    echo "Flask is not installed, installing..."
-    cat /etc/resolv.conf
-    echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
-    echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
-    sudo systemctl restart network-manager
-    sudo apt update
-    sudo $(which pip3) install --upgrade pip
-    sudo $(which pip3) install flask
-    # read -n 1 -s
-  else
-    echo "Flask is already installed"
-  fi
+  cat /etc/resolv.conf
+  echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+  echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
+  sudo systemctl restart network-manager
+  sudo apt update
+  sudo $(which pip3) install --upgrade pip
+  sudo $(which pip3) install flask
 
   # start manager
   cd system/manager
