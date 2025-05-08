6087f5d1f7d96606d7f735e7407dc220fa898370
CT921 <tim.chen242@gmail.com>
2025-05-08 18:32:28 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -162,10 +162,6 @@ class LongitudinalPlanner(LongitudinalPlannerTOP):
 
     self.mode = 'blended' if sm['selfdriveState'].experimentalMode else 'acc'
 
-    if self.mode != prev_mode:
-      print(f"Mode changed: {prev_mode} -> {self.mode}")
-    self.mpc.mode = 'blended' if sm['selfdriveState'].experimentalMode else 'acc'
-
     if len(sm['carControl'].orientationNED) == 3:
       accel_coast = get_coast_accel(sm['carControl'].orientationNED[1])
     else:
