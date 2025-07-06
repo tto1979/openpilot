2a1a75ef661296a86855d0c69f5c5f28e93d6771
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-07-06 15:45:41 +0800

    Revert "Update latcontrol_torque.py"
    This reverts commit a3f34533be4eae0d9c6fb516c49371bd237554d5.

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -27,7 +27,7 @@ LOW_SPEED_Y = [15, 13, 10, 5]
 LOW_SPEED_Y_NN = [12, 3, 1, 0]
 
 LAT_PLAN_MIN_IDX = 5
-LATERAL_LAG_MOD = 0.0
+LATERAL_LAG_MOD = 0.1
 
 def get_predicted_lateral_jerk(lat_accels, t_diffs):
   # compute finite difference between subsequent model_data.acceleration.y values
