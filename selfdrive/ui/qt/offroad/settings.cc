3591e1cd1983d55e79b800c243390fdfdacfcd39
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-07-28 22:00:23 +0800

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -679,7 +679,7 @@ TimpilotPanel::TimpilotPanel(QWidget* parent) : QWidget(parent) {
 
   toggles.append(new ParamControl("road_edge_detection",
                                   tr("Edge Detection During Lane Changes"),
-                                  tr("When the system detects obstacles at the vehicle's edge, lane change assist functionality will be temporarily suspended.\nNOTE: This will show 'Car Detected in Blindspot' warning.")"),
+                                  tr("When the system detects obstacles at the vehicle's edge, lane change assist functionality will be temporarily suspended.\nNOTE: This will show 'Car Detected in Blindspot' warning."),
                                   "../assets/icons/lane.png",
                                   this));
 
