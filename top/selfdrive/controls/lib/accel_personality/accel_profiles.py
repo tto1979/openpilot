84637baa8dba6f9ad0cb4ad318ddc5dbfe53cf00
CT921 <tim.chen242@gmail.com>
2025-08-04 14:14:08 +0800

    update brake
    This reverts commit 7d2a90b77ab2d39c9bfdb8b784698ebd8bab6893.

--- top/selfdrive/controls/lib/accel_personality/accel_profiles.py
+++ top/selfdrive/controls/lib/accel_personality/accel_profiles.py
@@ -16,12 +16,12 @@ MAX_ACCEL_PROFILES = {
 MAX_ACCEL_BREAKPOINTS = [0.,  1,   3.,  6.,   8.,  11.,  16., 20., 25., 30., 55.]
 
 MIN_ACCEL_PROFILES = {
-  "eco":                [-.07115, -.1713, -.272, -.372, -1.2, -1.2],
-  "normal":             [-.07117, -.1714, -.273, -.373, -1.2, -1.2],
-  "sport":              [-.07119, -.1715, -.274, -.374, -1.2, -1.2],
-  "stock":              [-1.2,    -1.2,   -1.2,  -1.2,  -1.2, -1.2],
+  "eco":                [-1.2, -1.2],
+  "normal":             [-1.2, -1.2],
+  "sport":              [-1.2, -1.2],
+  "stock":              [-1.2, -1.2],
 }
-MIN_ACCEL_BREAKPOINTS = [0.,       1.2,    8.,    14.,   22.2, 40.]
+MIN_ACCEL_BREAKPOINTS = [0.,    50.]
 
 # Precompute slopes for Cubic Bézier curves
 def compute_symmetric_slopes(x, y):
