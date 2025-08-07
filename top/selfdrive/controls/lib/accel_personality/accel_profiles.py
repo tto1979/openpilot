be365735e837fd33edfa1b85dee9cedc39718f55
CT921 <tim.chen242@gmail.com>
2025-08-07 11:45:12 +0800

    Update accel_profiles.py

--- top/selfdrive/controls/lib/accel_personality/accel_profiles.py
+++ top/selfdrive/controls/lib/accel_personality/accel_profiles.py
@@ -16,12 +16,12 @@ MAX_ACCEL_PROFILES = {
 MAX_ACCEL_BREAKPOINTS = [0.,  1,   3.,  6.,   8.,  11.,  16., 20., 25., 30., 55.]
 
 MIN_ACCEL_PROFILES = {
-  "eco":                [-.08, -.08, -.1, -1.10, -1.10],
-  "normal":             [-.09, -.09, -.2, -1.20, -1.20],
-  "sport":              [-.10, -.10, -.2, -1.30, -1.20],
-  "stock":              [-1.2, -1.2, -1.2, -1.2, -1.2],
+  "eco":                [-.08, -.2,  -.5,  -.8, -1.00, -1.10],
+  "normal":             [-.09, -.3,  -.6,  -.9, -1.05, -1.15],
+  "sport":              [-.10, -.4,  -.7,  -.95, -1.1, -1.20],
+  "stock":              [-1.2, -1.2, -1.2, -1.2, -1.2, -1.2],
 }
-MIN_ACCEL_BREAKPOINTS = [0.,   2.,   3.,  8.3, 50.]
+MIN_ACCEL_BREAKPOINTS = [0.,    1.,   2.,   3.,   8.3,  50.]
 
 # Precompute slopes for Cubic Bézier curves
 def compute_symmetric_slopes(x, y):
