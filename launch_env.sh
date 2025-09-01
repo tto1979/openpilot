491874d0ce7178c27c6cf85cad889e913281da76
CT921 <tim.chen242@gmail.com>
2025-09-01 12:09:48 +0800

    CarModel update

--- launch_env.sh
+++ launch_env.sh
@@ -5,6 +5,9 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
+if [ -s /data/params/d/CarModel ] && [ "$(cat /data/params/d/CarModel)" != "[-Not selected-]" ]; then
+  export FINGERPRINT="$(cat /data/params/d/CarModel)"
+fi
 
 if [ -z "$AGNOS_VERSION" ]; then
   export AGNOS_VERSION="12.8"
