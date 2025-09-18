31472e481b426d8f7b5cf8641b7ab7be9472b897
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-09-18 17:03:51 +0800

    drawRoadName
    Revert "update"
    
    This reverts commit 7cd55dab4c31f1832300cbb777d543c48fb536a1.
    
    Update hud.cc
    
    Update hud.cc
    
    Update hud.cc
    
    update
    
    Update hud.cc
    
    update
    
    Update process_config.py

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -147,7 +147,7 @@ UIState::UIState(QObject *parent) : QObject(parent) {
     "modelV2", "controlsState", "liveCalibration", "radarState", "deviceState",
     "pandaStates", "carParams", "driverMonitoringState", "carState", "driverStateV2",
     "wideRoadCameraState", "managerState", "selfdriveState", "longitudinalPlan",
-    "topControlsState", "longitudinalPlanTOP",
+    "topControlsState", "longitudinalPlanTOP", "liveMapDataTOP",
   });
   scene.wide_cam = false;
   scene.longitudinal_control = false;
