81b230540c1f0ad69f2138f683a591ca87562eb0
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-09-24 11:51:39 +0800

    Update vision_controller.py

--- top/selfdrive/controls/lib/smart_cruise_control/vision_controller.py
+++ top/selfdrive/controls/lib/smart_cruise_control/vision_controller.py
@@ -86,11 +86,12 @@ class SmartCruiseControlVision:
       rate_plan = np.array(np.abs(sm['modelV2'].orientationRate.z))
       vel_plan = np.array(sm['modelV2'].velocity.x)
 
-      self.current_lat_acc = float(self.v_ego ** 2 * abs(sm['controlsState'].curvature))
+      curvature = sm['controlsState'].curvature
+      self.current_lat_acc = float(self.v_ego ** 2 * abs(float(curvature)))
 
       # get the maximum lat accel from the model
       predicted_lat_accels = rate_plan * vel_plan
-      self.max_pred_lat_acc = float(np.amax(predicted_lat_accels))
+      self.max_pred_lat_acc = float(np.max(predicted_lat_accels))
 
       # get the maximum curve based on the current velocity
       v_ego = max(self.v_ego, 0.1)  # ensure a value greater than 0 for calculations
