a8375709d96fae4fa8fbe041d8a9e32e6ab9c998
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-10-02 13:47:20 +0800

    Update test_alerts.py

--- selfdrive/selfdrived/tests/test_alerts.py
+++ selfdrive/selfdrived/tests/test_alerts.py
@@ -47,7 +47,6 @@ class TestAlerts:
 
       cfg = [c for c in CONFIGS if c.proc_name == 'selfdrived'][0]
       cls.sm = SubMaster(cfg.pubs)
-      cls.sm.update_msgs(0, ['longitudinalPlanTOP'])
 
   def test_events_defined(self):
     # Ensure all events in capnp schema are defined in events.py
