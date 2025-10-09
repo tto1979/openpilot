f2ed792ce48c35384198c9c03d2b90a06940a51a
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-10-10 00:04:40 +0800

    For C3X Only, Update to T.O.P v0.10.0.2 DEV. - 2025.10.09
    NNV Model

---
+++ common/version.h
@@ -0,0 +1,1 @@
+#define COMMA_VERSION "0.10.0.2 DEV. - 2025.10.09 (NNV Model)"
---
+++ selfdrive/ui/qt/offroad/speed_limit/speed_limit_settings.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
+ *
+ * This file is part of sunnypilot and is licensed under the MIT License.
+ * See the LICENSE.md file in the root directory for more details.
+ */
+
+#pragma once
+
+#include "common/params.h"
+#include "selfdrive/ui/qt/offroad/speed_limit/helpers.h"
+#include "selfdrive/ui/qt/offroad/speed_limit/speed_limit_policy.h"
+#include "selfdrive/ui/qt/widgets/controls.h"
+#include "selfdrive/ui/qt/widgets/scrollview.h"
+#include <QStackedWidget>
+#include <QFrame>
+
+class SpeedLimitSettings : public QStackedWidget {
+  Q_OBJECT
+
+public:
+  SpeedLimitSettings(QWidget *parent = nullptr);
+  void refresh();
+  void showEvent(QShowEvent *event) override;
+
+signals:
+  void backPress();
+
+private:
+  Params params;
+  ScrollView *speedLimitScroller;
+  QFrame *subPanelFrame;
+  ButtonParamControl *speed_limit_mode_settings;
+  PushButton *speedLimitSource;
+  SpeedLimitPolicy *speedLimitPolicyScreen;
+  ButtonParamControl *speed_limit_offset_settings;
+  OptionControl *speed_limit_offset;
+
+  static QString offsetDescription(SpeedLimitOffsetType type = SpeedLimitOffsetType::NONE) {
+    QString none_str = tr("⦿ None: No Offset");
+    QString fixed_str = tr("⦿ Fixed: Adds a fixed offset [Speed Limit + Offset]");
+    QString percent_str = tr("⦿ Percent: Adds a percent offset [Speed Limit + (Offset % Speed Limit)]");
+
+    if (type == SpeedLimitOffsetType::FIXED) {
+      fixed_str = "<font color='white'><b>" + fixed_str + "</b></font>";
+    } else if (type == SpeedLimitOffsetType::PERCENT) {
+      percent_str = "<font color='white'><b>" + percent_str + "</b></font>";
+    } else {
+      none_str = "<font color='white'><b>" + none_str + "</b></font>";
+    }
+
+    return QString("%1<br>%2<br>%3")
+        .arg(none_str)
+        .arg(fixed_str)
+        .arg(percent_str);
+  }
+
+  static QString modeDescription(SpeedLimitMode mode = SpeedLimitMode::OFF) {
+    QString off_str = tr("⦿ Off: Disables the Speed Limit functions.");
+    QString info_str = tr("⦿ Information: Displays the current road's speed limit.");
+    QString warning_str = tr("⦿ Warning: Provides a warning when exceeding the current road's speed limit.");
+    QString assist_str = tr("⦿ Assist: Adjusts the vehicle's cruise speed based on the current road's speed limit when operating the +/- buttons.");
+
+    if (mode == SpeedLimitMode::ASSIST) {
+      assist_str = "<font color='white'><b>" + assist_str + "</b></font>";
+    } else if (mode == SpeedLimitMode::WARNING) {
+      warning_str = "<font color='white'><b>" + warning_str + "</b></font>";
+    } else if (mode == SpeedLimitMode::INFO) {
+      info_str = "<font color='white'><b>" + info_str + "</b></font>";
+    } else {
+      off_str = "<font color='white'><b>" + off_str + "</b></font>";
+    }
+
+    return QString("%1<br>%2<br>%3<br>%4")
+        .arg(off_str)
+        .arg(info_str)
+        .arg(warning_str)
+        .arg(assist_str);
+  }
+};
