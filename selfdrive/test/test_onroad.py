475aa4dd500d16547c6f02a05fb3663115e4001b
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-10-23 08:54:25 +0800

    Revert "test_onroad: ignore first few ui timing frames (#36385)"
    This reverts commit d71d2bd2d0a69977d71e58ea2b2ad589ae3d9f89.

--- selfdrive/test/test_onroad.py
+++ selfdrive/test/test_onroad.py
@@ -206,8 +206,7 @@ class TestOnroad:
     result += "-------------- UI Draw Timing ------------------\n"
     result += "------------------------------------------------\n"
 
-    # skip first few frames -- connecting to vipc
-    ts = self.ts['uiDebug']['drawTimeMillis'][10:]
+    ts = self.ts['uiDebug']['drawTimeMillis']
     result += f"min  {min(ts):.2f}ms\n"
     result += f"max  {max(ts):.2f}ms\n"
     result += f"std  {np.std(ts):.2f}ms\n"
