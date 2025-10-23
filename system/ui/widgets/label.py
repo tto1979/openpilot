a7c01ec4c656aff6d9b662a10b6224e2b9e6ebde
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-10-23 08:55:33 +0800

    Revert "raylib: fix full size alert text (#36379)"
    This reverts commit 65e1fd299e2fc085c5e3d5b32a366736df85de54.

--- system/ui/widgets/label.py
+++ system/ui/widgets/label.py
@@ -94,7 +94,6 @@ class Label(Widget):
                font_size: int = DEFAULT_TEXT_SIZE,
                font_weight: FontWeight = FontWeight.NORMAL,
                text_alignment: int = rl.GuiTextAlignment.TEXT_ALIGN_CENTER,
-               text_alignment_vertical: int = rl.GuiTextAlignmentVertical.TEXT_ALIGN_MIDDLE,
                text_padding: int = 0,
                text_color: rl.Color = DEFAULT_TEXT_COLOR,
                icon: Union[rl.Texture, None] = None,  # noqa: UP007
@@ -105,7 +104,6 @@ class Label(Widget):
     self._font = gui_app.font(self._font_weight)
     self._font_size = font_size
     self._text_alignment = text_alignment
-    self._text_alignment_vertical = text_alignment_vertical
     self._text_padding = text_padding
     self._text_color = text_color
     self._icon = icon
@@ -120,10 +118,6 @@ class Label(Widget):
   def set_text_color(self, color):
     self._text_color = color
 
-  def set_font_size(self, size):
-    self._font_size = size
-    self._update_text(self._text)
-
   def _update_layout_rects(self):
     self._update_text(self._text)
 
@@ -137,10 +131,7 @@ class Label(Widget):
 
   def _render(self, _):
     text_size = self._text_size[0] if self._text_size else rl.Vector2(0.0, 0.0)
-    if self._text_alignment_vertical == rl.GuiTextAlignmentVertical.TEXT_ALIGN_MIDDLE:
-      text_pos = rl.Vector2(self._rect.x, (self._rect.y + (self._rect.height - text_size.y) // 2))
-    else:
-      text_pos = rl.Vector2(self._rect.x, self._rect.y)
+    text_pos = rl.Vector2(self._rect.x, (self._rect.y + (self._rect.height - text_size.y) // 2))
 
     if self._icon:
       icon_y = self._rect.y + (self._rect.height - self._icon.height) / 2
