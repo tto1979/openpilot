28cbbf41834126cad9d85d7a9a0e4756676bff63
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-10-23 09:26:49 +0800

    revert agnos 14.0

--- launch_env.sh
+++ launch_env.sh
@@ -10,7 +10,7 @@ if [ -s /data/params/d/CarModel ] && [ "$(cat /data/params/d/CarModel)" != "[-No
 fi
 
 if [ -z "$AGNOS_VERSION" ]; then
-  export AGNOS_VERSION="14"
+  export AGNOS_VERSION="13.1"
 fi
 
 export STAGING_ROOT="/data/safe_staging"
