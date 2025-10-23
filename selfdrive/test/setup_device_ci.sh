defb06755117ee9fec8cd9eab00a83aefde1f168
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-10-23 08:42:34 +0800

    Revert "ci: stop power_monitor once"
    This reverts commit 727a750b3411773eb0873ecb5554a057af852f5f.

--- selfdrive/test/setup_device_ci.sh
+++ selfdrive/test/setup_device_ci.sh
@@ -42,7 +42,6 @@ sudo systemctl restart NetworkManager
 sudo systemctl disable ssh-param-watcher.path
 sudo systemctl disable ssh-param-watcher.service
 sudo mount -o ro,remount /
-sudo systemctl stop power_monitor
 
 while true; do
   if ! sudo systemctl is-active -q ssh; then
@@ -55,6 +54,7 @@ while true; do
   #  /data/ciui.py &
   #fi
 
+  sudo systemctl stop power_monitor
   sleep 5s
 done
 
