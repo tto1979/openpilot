8dc6d3fa292a36d09c541bfa6d190be5df98af54
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-11-26 08:24:30 +0800

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -30,8 +30,8 @@ INTERP_SPEEDS = [1, 1.5, 2.0, 3.0, 5, 7.5, 10, 15, 30]
 KP_INTERP = [250, 120, 65, 30, 11.5, 5.5, 3.5, 2.0, KP]
 
 # NNFF mode parameters (legacy)
-KP_NNFF = 0.7
-KI_NNFF = 0.15
+KP_NNFF = 0.6
+KI_NNFF = 0.1
 KD_NNFF = 0
 LOW_SPEED_X = [0, 10, 20, 30]
 LOW_SPEED_Y_NN = [12, 3, 1, 0]
