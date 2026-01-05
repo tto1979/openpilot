8543eae4448c820716e378542dc39a22f8236860
【TIM】 <90225659+CT921@users.noreply.github.com>
2026-01-05 15:15:12 +0800

    update

--- common/version.h
+++ common/version.h
@@ -1,1 +1,1 @@
-#define COMMA_VERSION "0.10.1.3 DEV. - 2025.12.31 (WMI Model)"
+#define COMMA_VERSION "0.10.1.3 DEV. - 2025.1.5 (WMI Model)"
--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -79,7 +79,7 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 1.45
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.95
+    return 0.9
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -102,11 +102,11 @@ def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
 
 def get_STOP_DISTANCE(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 5.5
-  elif personality==log.LongitudinalPersonality.standard:
     return 5.0
-  elif personality==log.LongitudinalPersonality.aggressive:
+  elif personality==log.LongitudinalPersonality.standard:
     return 4.5
+  elif personality==log.LongitudinalPersonality.aggressive:
+    return 4.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
