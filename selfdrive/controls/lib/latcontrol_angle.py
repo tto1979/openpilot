1abb78455d0cd335e7b2069441e3bf710735bee8
【TIM】 <90225659+CT921@users.noreply.github.com>
2026-02-23 20:49:33 +0800

    Update latcontrol_torque.py
    Update latcontrol_torque.py
    
    update
    
    update
    
    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_angle.py
+++ selfdrive/controls/lib/latcontrol_angle.py
@@ -13,7 +13,7 @@ class LatControlAngle(LatControl):
     self.sat_check_min_speed = 5.
     self.use_steer_limited_by_safety = CP.brand == "tesla"
 
-  def update(self, active, CS, VM, params, steer_limited_by_safety, desired_curvature, curvature_limited, lat_delay, model_data=None):
+  def update(self, active, CS, VM, params, steer_limited_by_safety, desired_curvature, calibrated_pose, curvature_limited, lat_delay, model_data=None):
     angle_log = log.ControlsState.LateralAngleState.new_message()
 
     if not active:
