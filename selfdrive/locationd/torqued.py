f1240f74a8f70343c9897941a732ab8dd5bb9c8a
【TIM】 <90225659+CT921@users.noreply.github.com>
2026-05-10 09:32:02 +0800

    Update torqued.py

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -98,7 +98,7 @@ class TorqueEstimator(ParameterEstimator):
     params = Params()
     params_cache = params.get("CarParamsPrevRoute")
     torque_cache = params.get("LiveTorqueParameters")
-    if params_cache is not None and torque_cache is not None and not params.get_bool("DisableLagLearning"):
+    if params_cache is not None and torque_cache is not None and not params.get_bool("NNFF"):
       try:
         with log.Event.from_bytes(torque_cache) as log_evt:
           cache_ltp = log_evt.liveTorqueParameters
@@ -157,7 +157,7 @@ class TorqueEstimator(ParameterEstimator):
     return slope, offset, friction_coeff
 
   def update_params(self, params):
-    if Params().get_bool("DisableLagLearning"):
+    if Params().get_bool("NNFF"):
       return
 
     self.decay = min(self.decay + DT_MDL, MAX_FILTER_DECAY)
